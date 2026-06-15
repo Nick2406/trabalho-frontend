433aa4a410311c25b12a306e5d3bdfbe29b8331d
Nicolas Vital <nickfv2406@gmail.com>
2026-06-15 20:17:53 -0300

    feat: finaliza tela inicial, listagem de musicas e documentacao (closes #7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import RotaPrivada from './components/RotaPrivada';
 import Inicio from './pages/Inicio';
 import Cadastro from './pages/Cadastro';
-import Historico from './pages/Historico';
+import NovoScrobble from './pages/NovoScrobble';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Fundo escuro global ocupando a tela toda */}
       <div className="min-h-screen bg-zinc-950 font-sans">
         <Navbar />
-        
         <main className="max-w-5xl mx-auto w-full">
           <Routes>
-            <Route path="/" element={<Inicio />} />
+            <Route 
+              path="/novo-scrobble"
+              element={
+                <RotaPrivada>
+                  <NovoScrobble />
+                </RotaPrivada>
+              } 
+            />
+            <Route 
+              path="/" 
+              element={
+                <RotaPrivada>
+                  <Inicio />
+                </RotaPrivada>
+              } 
+            />
             <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/historico" element={<Historico />} />
           </Routes>
         </main>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
-  // Função para aplicar cor vermelha se o link estiver ativo, ou branco se não estiver
   const linkStyles = ({ isActive }) =>
     isActive
       ? "text-red-500 font-semibold border-b-2 border-red-500 pb-1"
@@ -9,15 +8,15 @@ export default function Navbar() {
 
   return (
     <header className="bg-black py-4 px-8 shadow-md flex flex-col md:flex-row justify-between items-center border-b border-zinc-800">
-      {/* Logo Fake */}
       <div className="text-2xl font-black text-white tracking-tighter mb-4 md:mb-0">
         last<span className="text-red-600">.fm</span>
       </div>
-
-      {/* Menu de Navegação */}
       <nav className="flex gap-6 text-sm uppercase tracking-wide font-medium">
         <NavLink to="/" className={linkStyles}>
           Início
+        </NavLink>
+        <NavLink to="/novo-scrobble" className={linkStyles}>
+          Novo Scrobble
         </NavLink>
         <NavLink to="/cadastro" className={linkStyles}>
           Cadastro
